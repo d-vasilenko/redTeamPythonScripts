e258fd69e084f787985e1d0629680429fce35049
ghostinchip <ghostinchip@gmail.com>
2026-07-06 21:15:13 +0300

    refactor: add tests and fix docstrings

--- port_checker.py
+++ port_checker.py
@@ -4,55 +4,93 @@
 2. Прочитайте файл `ips.txt` (каждый IP с новой строки) и для каждого IP проверьте порт 80, запишите открытые IP в файл `open_ips.txt`.
 """
 
+import logging
 import socket
-# from datetime import datetime as dt
+from typing import Tuple
+
+logging.basicConfig(level=logging.INFO)
 
 def read_ip_list_from_file(file: str) -> list:
-    """Функция читает список ip адресов из файла"""
+    """
+    Функция читает список ip адресов из файла.
+
+    Args:
+        file (str): имя файла из которого читаем
+    Returns:
+        list: список IP адресов
+    Raises:
+        FileNotFoundError: Если файл не найден.
+        PermissionError: Если нет прав для чтения файла.
+        UnicodeDecodeError: Если проблемы с кодиравкой файла.
+    """
     ip_list = []
     try:
         with open(file, 'r', encoding="utf-8") as f:
             for line in f:
                 ip_list.append(line.strip())
     except FileNotFoundError:
-        print("Ошибка: Файл не найден!")
+        logging.error("Ошибка: Файл не найден!")
     except PermissionError:
-        print("Ошибка: Нет прав для чтения файла!")
+        logging.error("Ошибка: Нет прав для чтения файла!")
     except UnicodeDecodeError:
-        print("Ошибка: Проблемма с кодировкой файла!")
+        logging.error("Ошибка: Проблемма с кодировкой файла!")
     except Exception as e:
-        print(f"Неизвестная ошибка: {e}")
+        logging.error(f"Неизвестная ошибка: {e}")
     return ip_list
 
 def write_checked_ip(file: str, data: list) -> bool:
-    """Функция записывает в файл хосты с открытым портом"""
+    """
+    Функция записывает в файл хосты с открытым портом.
+
+    Args: 
+        file (str): имя файла, в который пишем найденные хосты.
+        data (list): список хостов с открытым портом.
+    Returns:
+        bool: Произоша запись файла или нет.
+    Raises:
+        PermissionError: Если нет прав для записи файла.
+        IsADirectoryError: Если путь являеться директорией.
+        OSError: Если произошла ошибка при записи файла.
+        UnicodeEncodeError: Если произошла ошибка кодировки.
+    """
     try:
         with open(file, 'w', encoding="utf-8") as f:
             f.writelines(f"{line[0]}:{line[1]} {line[2]}\n" for line in data)
-        print(f"Данные проверки успешно записаны в файл {file}")
+        logging.info(f"Данные проверки успешно записаны в файл {file}")
         return True
     except PermissionError:
-        print("Нет прав для записи файла!")
+        logging.error("Нет прав для записи файла!")
     except IsADirectoryError:
-        print("Путь являеться директорией!")
+        logging.error("Путь являеться директорией!")
     except OSError as e:
-        print(f"Ошибка ОС при записи в {file}: {e}")
+        logging.error(f"Ошибка ОС при записи в {file}: {e}")
     except UnicodeEncodeError as e:
-        print(f"Ошибкаа кодировки: {e}")
+        logging.error(f"Ошибкаа кодировки: {e}")
     except Exception as e:
-        print(f"Неожиданная ошибка: {e}")
+        logging.error(f"Неожиданная ошибка: {e}")
     
     return False
 
-def check_port(host: str, port: int, timeout: int=3) -> tuple:
-    """Функция проверяет хост на открытый порт"""
+def check_port(host: str, port: int, timeout: int=3) -> Tuple[str, int, str]:
+    """
+    Функция проверяет доступность TCP-порта на указанном хосте.
+
+    Args:
+        host (str): IP-адрес целевого хоста.
+        port (int): Номер TCP-порта для проверки (например 80).
+        timeout (int, optional): Время ожидания ответа в секундах, по умолчанию 3.
+    Returns:
+        Tuple[str, int, str]: Кортеж вида (host, port, status), где status может быть 'open', 'closed', 'invalid'.
+    Raises:
+        socket.gaierror: Если передан не валидный IP-адрес. 
+    """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.settimeout(timeout)
 
         try:
             result = client.connect_ex((host, port))
         except socket.gaierror as e:
-            print(f"Ошибка разрешения ip адреса {e}")
+            logging.error(f"Ошибка разрешения ip адреса {e}")
             return (host, port, 'invalid')
 
         if result == 0:
@@ -61,7 +99,6 @@ def check_port(host: str, port: int, timeout: int=3) -> tuple:
             status = 'closed'
     
     return (host, port, status)
-
 
 
 def main():
@@ -73,9 +110,17 @@ def main():
         if line[2] == 'open':
             result_list.append(line)
     
-    print(result_list)
+    logging.info(result_list)
 
     write_checked_ip("open_hosts.txt", result_list)    
 
 if __name__ == "__main__":
+    
+    # tests
+    check_port("127.0.0.1", 22)
+    check = check_port("abc", 80)
+    logging.info(check)
+    check = check_port("192.168.10.60", 80, timeout=2)
+    logging.info(check)
+
     main()
